f9b7e7260a66a89eb93b54a5a80b2df0c079bac2
Krzysztof Ruta <krzysztof.ruta@play.pl>
2022-04-01 20:11:57 +0200

    simpify flattening of maps

--- src/main/scala/org/kr/scala/z80/Load8Bit.scala
+++ src/main/scala/org/kr/scala/z80/Load8Bit.scala
@@ -2,35 +2,6 @@ package org.kr.scala.z80
 
 object Load8Bit {
   // Z80 manual page 42
-  val destRegListMap:Map[List[OpCode],String]=Map(
-    List(OpCode(0xED,0x57),OpCode(0xED,0x5F),OpCode(0x7F,OpCode.ANY),OpCode(0x78,OpCode.ANY),OpCode(0x79,OpCode.ANY),
-      OpCode(0x7A,OpCode.ANY),OpCode(0x7B,OpCode.ANY),OpCode(0x7C,OpCode.ANY),OpCode(0x7D,OpCode.ANY),
-      OpCode(0x7E,OpCode.ANY),OpCode(0x7F,OpCode.ANY),OpCode(0x0A,OpCode.ANY),OpCode(0x1A,OpCode.ANY),OpCode(0xDD,0x7E),
-      OpCode(0xFD,0x7E),OpCode(0x3A,OpCode.ANY),OpCode(0x3E,OpCode.ANY)) -> "A",
-    List(OpCode(0x47,OpCode.ANY),OpCode(0x40,OpCode.ANY),OpCode(0x41,OpCode.ANY),OpCode(0x42,OpCode.ANY),
-      OpCode(0x43,OpCode.ANY),OpCode(0x44,OpCode.ANY),OpCode(0x45,OpCode.ANY),
-      OpCode(0x46,OpCode.ANY),OpCode(0xDD,0x46),OpCode(0xFD,0x46),OpCode(0x06,OpCode.ANY)) ->"B",
-    List(OpCode(0x4F,OpCode.ANY),OpCode(0x48,OpCode.ANY),OpCode(0x49,OpCode.ANY),OpCode(0x4A,OpCode.ANY),
-      OpCode(0x4B,OpCode.ANY),OpCode(0x4C,OpCode.ANY),OpCode(0x4D,OpCode.ANY),
-      OpCode(0x4E,OpCode.ANY),OpCode(0xDD,0x4E),OpCode(0xFD,0x4E),OpCode(0x0E,OpCode.ANY)) ->"C",
-    List(OpCode(0x57,OpCode.ANY),OpCode(0x50,OpCode.ANY),OpCode(0x51,OpCode.ANY),OpCode(0x52,OpCode.ANY),
-      OpCode(0x53,OpCode.ANY),OpCode(0x54,OpCode.ANY),OpCode(0x55,OpCode.ANY),
-      OpCode(0x56,OpCode.ANY),OpCode(0xDD,0x56),OpCode(0xFD,0x56),OpCode(0x16,OpCode.ANY)) ->"D",
-    List(OpCode(0x5F,OpCode.ANY),OpCode(0x58,OpCode.ANY),OpCode(0x59,OpCode.ANY),OpCode(0x5A,OpCode.ANY),
-      OpCode(0x5B,OpCode.ANY),OpCode(0x5C,OpCode.ANY),OpCode(0x5D,OpCode.ANY),
-      OpCode(0x5E,OpCode.ANY),OpCode(0xDD,0x5E),OpCode(0xFD,0x5E),OpCode(0x1E,OpCode.ANY)) ->"E",
-    List(OpCode(0x67,OpCode.ANY),OpCode(0x60,OpCode.ANY),OpCode(0x61,OpCode.ANY),OpCode(0x62,OpCode.ANY),
-      OpCode(0x63,OpCode.ANY),OpCode(0x64,OpCode.ANY),OpCode(0x65,OpCode.ANY),
-      OpCode(0x66,OpCode.ANY),OpCode(0xDD,0x66),OpCode(0xFD,0x66),OpCode(0x26,OpCode.ANY)) ->"H",
-    List(OpCode(0x6F,OpCode.ANY),OpCode(0x68,OpCode.ANY),OpCode(0x69,OpCode.ANY),OpCode(0x6A,OpCode.ANY),
-      OpCode(0x6B,OpCode.ANY),OpCode(0x6C,OpCode.ANY),OpCode(0x6D,OpCode.ANY),
-      OpCode(0x6E,OpCode.ANY),OpCode(0xDD,0x6E),OpCode(0xFD,0x6E),OpCode(0x2E,OpCode.ANY)) ->"L",
-    List(OpCode(0xED,0x47))->"I",
-    List(OpCode(0xED,0x4F))->"R"
-  )
-  val destReg:Map[OpCode,String]=destRegListMap.map(entry=>entry._1.flatMap(opcode=>Map(opcode->entry._2))).flatten.toMap
-  def getDestReg(opcode:OpCode):String = destReg.getOrElse(opcode,destReg(OpCode(opcode.main,OpCode.ANY)))
-
   val sourceLocListMap:Map[List[OpCode],LocationSpec8Bit]=Map(
     // registers
     List(OpCode(0xED,0x57)) -> LocationSpec8Bit.register("I"),
@@ -76,7 +47,7 @@ object Load8Bit {
     List(OpCode(0xDD,0x36),OpCode(0xFD,0x36)) -> LocationSpec8Bit.registerAddrDirOffset("PC",3)
   )
 
-  val sourceLoc:Map[OpCode,LocationSpec8Bit]=sourceLocListMap.map(entry=>entry._1.flatMap(opcode=>Map(opcode->entry._2))).flatten.toMap
+  val sourceLoc:Map[OpCode,LocationSpec8Bit]=Z80Utils.flattenMapOfLists(sourceLocListMap)
   def getSourceLoc(opcode:OpCode):LocationSpec8Bit = sourceLoc.getOrElse(opcode,sourceLoc(OpCode(opcode.main,OpCode.ANY)))
 
   val destLocListMap:Map[List[OpCode],LocationSpec8Bit]=Map(
@@ -120,7 +91,7 @@ object Load8Bit {
     List(OpCode(0x32,OpCode.ANY)) -> LocationSpec8Bit.indirAddress(1)
   )
 
-  val destLoc:Map[OpCode,LocationSpec8Bit]=destLocListMap.map(entry=>entry._1.flatMap(opcode=>Map(opcode->entry._2))).flatten.toMap
+  val destLoc:Map[OpCode,LocationSpec8Bit]=Z80Utils.flattenMapOfLists(destLocListMap)
   def getDestLoc(opcode:OpCode):LocationSpec8Bit = destLoc.getOrElse(opcode,destLoc(OpCode(opcode.main,OpCode.ANY)))
 
 
@@ -158,12 +129,11 @@ object Load8Bit {
     List(OpCode(0xDD,0x36),OpCode(0xFD,0x36)) -> 4
   )
 
-  val instructionSize:Map[OpCode,Int]=instructionSizeListMap.map(entry=>entry._1.flatMap(opcode=>Map(opcode->entry._2))).flatten.toMap
+  val instructionSize:Map[OpCode,Int]=Z80Utils.flattenMapOfLists(instructionSizeListMap)
   val getInstructionSize: OpCode=>Int = opcode => instructionSize.getOrElse(opcode,instructionSize(OpCode(opcode.main,OpCode.ANY)))
 
-  val isLoad8Bit: OpCode=>Boolean = opcode => {
+  val isLoad8Bit: OpCode=>Boolean = opcode =>
     instructionSize.contains(opcode) || instructionSize.contains(OpCode(opcode.main,OpCode.ANY))
-  }
 }
 
 case class LocationSpec8Bit(reg:String, immediate:Int, offsetPC:Int, addressReg:String, directOffset:Int, indirectOffset:Int) {
