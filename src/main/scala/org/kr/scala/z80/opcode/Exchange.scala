7f749787779293df94dd5af5582d6e22d06a5d6a
Krzysztof Ruta <krzysztof.ruta@play.pl>
2022-04-04 09:31:19 +0200

    add EX (SP),IX/IY - finish exchange group

--- src/main/scala/org/kr/scala/z80/opcode/Exchange.scala
+++ src/main/scala/org/kr/scala/z80/opcode/Exchange.scala
@@ -13,13 +13,17 @@ object Exchange extends OperationSpec {
     List(OpCode(0x08, OpCode.ANY)) -> List(ExchangeLocation("AF",reg1Indirect=false,"AF1")),
     List(OpCode(0xD9, OpCode.ANY)) -> List(ExchangeLocation("BC",reg1Indirect=false,"BC1"),
       ExchangeLocation("DE",reg1Indirect=false,"DE1"),ExchangeLocation("HL",reg1Indirect=false,"HL1")),
+    //register with memory at SP
     List(OpCode(0xE3, OpCode.ANY)) -> List(ExchangeLocation("SP",reg1Indirect=true,"HL")),
+    List(OpCode(0xDD, 0xE3)) -> List(ExchangeLocation("SP",reg1Indirect=true,"IX")),
+    List(OpCode(0xFD, 0xE3)) -> List(ExchangeLocation("SP",reg1Indirect=true,"IY"))
   )
   val exchangeLoc: OpCodeMap[List[ExchangeLocation]] = new OpCodeMap(exchangeListMap, List(ExchangeLocation.empty))
 
   val instructionSizeListMap: Map[List[OpCode], Int] = Map(
     List(OpCode(0xEB, OpCode.ANY),OpCode(0x08, OpCode.ANY),OpCode(0xD9, OpCode.ANY),
-      OpCode(0xE3, OpCode.ANY)) -> 1
+      OpCode(0xE3, OpCode.ANY)) -> 1,
+    List(OpCode(0xDD, 0xE3),OpCode(0xFD, 0xE3)) -> 2
   )
   override val instSize: OpCodeMap[Int] = new OpCodeMap(instructionSizeListMap, 0)
 }
--- src/test/scala/org/kr/scala/z80/test/OpExchangeTest.scala
+++ src/test/scala/org/kr/scala/z80/test/OpExchangeTest.scala
@@ -103,4 +103,48 @@ class OpExchangeTest extends AnyFunSuite {
     //println(sysTest.get.memoryController.get.mem.slice(0,300))
     //println(sysTest.get.registerController.get.reg)
   }
+
+  test("run EX (SP),IX") {
+    //given
+    val sysBlank = Z80SystemController.blank
+    val reg = sysBlank.get.registerController >>=
+      RegisterController.set("IX",0xB1B2) >>=
+      RegisterController.set("SP",0x0103)
+    val mem = sysBlank.get.memoryController >>=
+      MemoryController.pokeMulti(0,Vector(0xDD,0xE3)) >>= // EX (SP),IX
+      MemoryController.pokeMulti(0x0103,Vector(0xE1,0xE2))
+    //when
+    val sysInit = Z80SystemController(new Z80System(MemoryController(mem.get), RegisterController(reg.get)))
+    val sysTest = sysInit >>= Z80SystemController.run(1)
+    //then
+    assert(sysTest.get.registerController.get("PC") == 2)
+    assert(sysTest.get.registerController.get("IX") == 0xE2E1)
+    assert(sysTest.get.registerController.get("SP") == 0x0103)
+    assert(sysTest.get.memoryController.get(0x0103)==0xB2)
+    assert(sysTest.get.memoryController.get(0x0104)==0xB1)
+    //println(sysTest.get.memoryController.get.mem.slice(0,300))
+    //println(sysTest.get.registerController.get.reg)
+  }
+
+  test("run EX (SP),IY") {
+    //given
+    val sysBlank = Z80SystemController.blank
+    val reg = sysBlank.get.registerController >>=
+      RegisterController.set("IY",0xB1B2) >>=
+      RegisterController.set("SP",0x0103)
+    val mem = sysBlank.get.memoryController >>=
+      MemoryController.pokeMulti(0,Vector(0xFD,0xE3)) >>= // EX (SP),IY
+      MemoryController.pokeMulti(0x0103,Vector(0xE1,0xE2))
+    //when
+    val sysInit = Z80SystemController(new Z80System(MemoryController(mem.get), RegisterController(reg.get)))
+    val sysTest = sysInit >>= Z80SystemController.run(1)
+    //then
+    assert(sysTest.get.registerController.get("PC") == 2)
+    assert(sysTest.get.registerController.get("IY") == 0xE2E1)
+    assert(sysTest.get.registerController.get("SP") == 0x0103)
+    assert(sysTest.get.memoryController.get(0x0103)==0xB2)
+    assert(sysTest.get.memoryController.get(0x0104)==0xB1)
+    //println(sysTest.get.memoryController.get.mem.slice(0,300))
+    //println(sysTest.get.registerController.get.reg)
+  }
 }
