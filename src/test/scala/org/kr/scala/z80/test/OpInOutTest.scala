899e73889614039b16a04b6e90de0669198a6a0e
Krzysztof Ruta <krzysztof.ruta@play.pl>
2022-04-13 08:26:32 +0200

    add OUT (C),r

--- src/test/scala/org/kr/scala/z80/test/OpInOutTest.scala
+++ src/test/scala/org/kr/scala/z80/test/OpInOutTest.scala
@@ -38,4 +38,35 @@ class OpInOutTest extends AnyFunSuite {
     //systemC.get.outputController.get.print(0xFF)
   }
 
+  test("run OUT (C),r - multiple") {
+    //given
+    //when
+    val systemC=TestUtils.prepareTest(List(),
+      List(
+        (0x0000,0x0E),(0x0001,0x80), // LD C,0x80
+        (0x0002,0x3E),(0x0003,0x41), // LD A,'A'
+        (0x0004,0xED),(0x0005,0x79), // OUT (C),A
+        (0x0006,0x06),(0x0007,0x42), // LD B,'B'
+        (0x0008,0xED),(0x0009,0x41), // OUT (C),B
+        (0x000A,0x16),(0x000B,0x43), // LD D,'C'
+        (0x000C,0xED),(0x000D,0x51), // OUT (C),D
+        (0x000E,0x1E),(0x000F,0x44), // LD E,'D'
+        (0x0010,0xED),(0x0011,0x59), // OUT (C),E
+        (0x0012,0x26),(0x0013,0x45), // LD H,'E'
+        (0x0014,0xED),(0x0015,0x61), // OUT (C),H
+        (0x0016,0x2E),(0x0017,0x46), // LD L,'F'
+        (0x0018,0xED),(0x0019,0x69)  // OUT (C),L
+      ),13)
+    //then
+    assert(systemC.get.registerController.get("PC")==0x001A)
+    assert(systemC.get.outputController.get(0x80,0)==0x41)
+    assert(systemC.get.outputController.get(0x80,1)==0x42)
+    assert(systemC.get.outputController.get(0x80,2)==0x43)
+    assert(systemC.get.outputController.get(0x80,3)==0x44)
+    assert(systemC.get.outputController.get(0x80,4)==0x45)
+    assert(systemC.get.outputController.get(0x80,5)==0x46)
+
+    //systemC.get.outputController.get.print(0xFF)
+  }
+
 }
