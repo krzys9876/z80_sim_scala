15a9cb2a23062f4a4c91b4fb90d03f86d31e8786
Krzysztof Ruta <krzysztof.ruta@play.pl>
2023-02-27 19:56:02 +0100

    add example programs

--- run.bat
+++ run.bat
@@ -1,6 +1,7 @@
 echo off
 cls
-java.exe -XX:+UseSerialGC -jar target\scala-2.13\z80_sim-assembly-0.3.jar input-files\basicall_KR_simpleIO_02.hex
+rem java.exe -XX:+UseSerialGC -jar target\scala-2.13\z80_sim-assembly-0.3.jar input-files\basicall_KR_simpleIO_02.hex
+java.exe -XX:+UseSerialGC -jar target\scala-2.13\z80_sim-assembly-0.3.jar input-files\basicall_KR_simpleIO_02.hex input-files\waves.txt 250000000
 rem java.exe -XX:+UseSerialGC -jar target\scala-2.13\z80_sim-assembly-0.3.jar input-files\basicall_KR_simpleIO_02.hex input-files\tictactoe10.txt 3600000000
 rem java.exe -XX:+UseSerialGC -jar target\scala-2.13\z80_sim-assembly-0.3.jar input-files\basicall_KR_simpleIO_02.hex input-files\input.txt 450000
 rem java.exe -XX:+UseSerialGC -jar target\scala-2.13\z80_sim-assembly-0.3.jar input-files\basicall_KR_simpleIO_02.hex input-files\arithmetic.txt 1400000
